e6885262327bc33d3b7b3e9b3fe9fa473a1ef4de
Nikita Nazarov <nnazarov@gmcs.ru>
2021-04-15 15:58:59 +0300

    add commit Figure

--- FigureLibrary/FigureLibrary/Figure.cs
+++ FigureLibrary/FigureLibrary/Figure.cs
@@ -3,7 +3,8 @@
 namespace FigureLibrary
 {
     // хотел Figure объявить интерфейсом, но потом решил сделать abstract class
-    // чтобы показать работу с protected
+    // чтобы показать работу с protected и добавил вычисление синуса
+    // если Figure оставил интерфейсом, то метода Sin вынес бы в хелпер 
     public abstract class Figure
     {
         public abstract double Area();
