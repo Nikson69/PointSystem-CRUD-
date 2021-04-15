501197c3bb5ad4ff988cc36b7860440a4a36b756
Nikita Nazarov <nnazarov@gmcs.ru>
2021-04-15 14:51:49 +0300

    add sql

--- FigureLibrary/SQL/Class1.sql
+++ FigureLibrary/SQL/Class1.sql
@@ -1,8 +1,23 @@
-﻿using System;
+﻿--Подготовка данных
+CREATE TABLE #Product (
+id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+  name char(50) NOT NULL,
+  categoryId uniqueidentifier)
 
-namespace SQL
-{
-    public class Class1
-    {
-    }
-}
+CREATE TABLE #Category (id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+  name char(50) NOT NULL)
+
+  DECLARE @id1 UNIQUEIDENTIFIER, @id2 UNIQUEIDENTIFIER
+
+SET @id1  = NEWID();
+set @id2 = NEWID();
+
+INSERT INTO #Category VALUES (@id1, 'Category1'), (@id2, 'Category2')
+
+INSERT INTO #Product VALUES (DEFAULT, 'Product1', @id2), (DEFAULT, 'Product2', @id1), (DEFAULT, 'Product3', @id2), (DEFAULT, 'Product4', null)
+
+
+--Требуемый результат
+select P.name as ProductName, C.name as CategoryName from #Product as P
+left join #Category as C on C.id = P.categoryId 
+Order by CategoryName 
